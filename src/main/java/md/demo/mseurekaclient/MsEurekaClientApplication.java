2172766665df37409f3afea32fe4ef6b303f4ab9
demo.lim <demo.lim@kakaocorp.com>
2020-12-02 00:05:30 +0900

    first commit

---
+++ src/main/java/md/demo/mseurekaclient/MsEurekaClientApplication.java
@@ -0,0 +1,13 @@
+package md.demo.mseurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsEurekaClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/md/demo/mseurekaclient/MsEurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package md.demo.mseurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEurekaClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
