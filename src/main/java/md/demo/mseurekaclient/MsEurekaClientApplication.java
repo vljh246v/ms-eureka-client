f55ba9e99b4ae8e9a63875a8360bb14fe5afbfda
demo.lim <demo.lim@kakaocorp.com>
2020-12-02 00:24:46 +0900

    client 설정
    * @EnableDiscoveryClient 추가
    * application.yml 추가

--- src/main/java/md/demo/mseurekaclient/MsEurekaClientApplication.java
+++ src/main/java/md/demo/mseurekaclient/MsEurekaClientApplication.java
@@ -2,8 +2,10 @@ package md.demo.mseurekaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsEurekaClientApplication {
 
     public static void main(String[] args) {
